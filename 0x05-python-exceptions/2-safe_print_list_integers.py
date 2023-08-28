62c10600c6cb2d48ec04bb1fc17803be02c36dc4
superUserT <rantshothabisomail@gmail.com>
2023-08-28 23:17:39 +0200

    print in propper format

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,6 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     countVar = 0
     for i in range(x):
         try:
+            print("{:d}".format(my_list[i]), end="")
             countVar += 1
         except (IndexError, ValueError):
             return pass
