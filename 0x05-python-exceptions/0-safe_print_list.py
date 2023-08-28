3f1a97f8c13d641c2c561c344f1cf05dfa8c546b
superUserT <rantshothabisomail@gmail.com>
2023-08-28 13:43:35 +0200

    0 done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-        try: 
-            for i in range(x):
-                print(my_list[i], end=" ")
-                return x
-        except ValueError:
-            print("Oops!  That was no valid number.  Try again...")
-            return len(my_list)
-
+    countVar = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            countVar += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return countVar
