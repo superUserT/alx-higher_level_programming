95262768ef31579619ef45fc1ba9e9d99cbd18a4
superUserT <rantshothabisomail@gmail.com>
2023-08-28 23:26:02 +0200

    loop inside

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     countVar = 0
-    for i in range(x):
-        try:
+    try:
+        for i in range(x):
             print("{:d}".format(my_list[i]), end="")
             countVar += 1
-        except (IndexError, ValueError):
-            pass
-    return countVar
+    except (IndexError, ValueError):
+        pass
+    finally:
+        print()
+        return countVar
