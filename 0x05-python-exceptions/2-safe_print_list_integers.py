2885bebd52d4bc8058446b9f2724c184afc18fb1
superUserT <rantshothabisomail@gmail.com>
2023-08-28 22:56:39 +0200

    2 init

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    countVar = 0
+    try: 
+        for i in range(x):
+            print("{:d}".format(my_list[i], end=" "))
+            countVar += 1
+    except (IndexError, ValueError):
+        return False
+    return True
