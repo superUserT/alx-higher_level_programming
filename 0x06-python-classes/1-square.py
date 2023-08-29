1a45614ac0378346e57d4e46cbf76e5a53b6aaf4
superUserT <rantshothabisomail@gmail.com>
2023-08-29 17:38:16 +0200

    comments 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""defines a square by its size"""
 class Square:
+    """Class thats a  square"""
     def __init__(self, size):
+        """
+        Args:
+            size (int): size of the square
+        """
         self.__size = size
