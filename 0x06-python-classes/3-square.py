1a34e78a8f90c73f0d2631bd01053fd7d1fe5c07
superUserT <rantshothabisomail@gmail.com>
2023-08-29 19:34:21 +0200

    3 square init

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Class that defines a square by its size"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """Validation
+        Args:
+            size (int): square size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
+    """Represents the area of a square"""
+    def area(self):
+        """Returns the square of a square"""
+        return self.__size **2
