9b7e88ff64cdeb4065b907df330e99ff49971f7a
superUserT <rantshothabisomail@gmail.com>
2023-08-29 19:54:12 +0200

    4-main.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,19 +5,23 @@
 class Square:
     """Represents a square"""
     def __init__(self, size=0):
-        """Validation
-        Args:
-            size (int): square size
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        """Validation"""
         self.__size = size
-
-
+    @property
+    def size(self):
+        """Get the square size."""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """Set the square size."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
     """Represents the area of a square"""
     def area(self):
         """Returns the square of a square"""
-        return self.__size **2
+        return self.__size ** 2
+    
 
