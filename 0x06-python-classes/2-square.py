ad565152c1689e7f4b3b13519c60fe6e131e661d
superUserT <rantshothabisomail@gmail.com>
2023-08-29 17:40:40 +0200

    2square comments

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Class that defines a square by its size"""
 class Square:
     def __init__(self, size=0):
+        """
+        Type and value validation in if else tree
+        
+        Args:
+            size (int): square size
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
