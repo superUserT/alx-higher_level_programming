9176e526a28e2716f52f53170751e305c2249285
superUserT <rantshothabisomail@gmail.com>
2023-09-06 23:26:55 +0200

    5 and 6

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+Module Name: 0-rectangle
+
+Module Description:
+This module contains only one class
+
+Module Classes:
+- Rectangle
+
+Module Attributes:
+- None
+"""
+
+
+class Rectangle:
+    """Empty class to represent a rectangle"""
+
+    def __init__(self, width: int = 0, height: int = 0):
+        # Vallidations
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self) -> int:
+        return self.__width
+
+    @width.setter
+    def width(self, value) -> None:
+        # Vallidations
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self) -> int:
+        return self.__height
+
+    @height.setter
+    def height(self, value) -> None:
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self) -> int:
+        return self.__height * self.__width
+
+    def perimeter(self) -> int:
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self) -> str:
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            return "\n".join(["#" * self.__width] * self.__height)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
