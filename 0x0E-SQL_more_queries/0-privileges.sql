86e22c657e7eed0c324d809b29349f35fac7e735
superUserT <rantshothabisomail@gmail.com>
2023-10-18 23:13:39 +0200

    sql queries

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- showing grants from some users of localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Creating table
+CREATE TABLE IF NOT EXISTS unique_id(
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
